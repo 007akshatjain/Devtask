021fe2700b7f8088368af7f65e08a840cbaf16dc
007akshatjain <97514385+007akshatjain@users.noreply.github.com>
2025-07-27 20:53:31 +0530

    Fix: Updated backend

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors({ origin: "https://devtask-gilt.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://devtask-gilt.vercel.app", credentials: true }));
 app.use(express.json());
 
 // Routes
